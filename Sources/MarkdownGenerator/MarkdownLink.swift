416577efe9a199260531630f40f0f758ddf8f644
yyokii <yyokii.h@gmail.com>
2021-11-30 19:44:53 +0900

    ✨ Add md link

---
+++ Sources/MarkdownGenerator/MarkdownLink.swift
@@ -0,0 +1,16 @@
+//
+//  MarkdownLink.swift
+//  
+//
+//  Created by Higashihara Yoki on 2021/11/30.
+//
+
+public struct MarkdownLink: MarkdownContent {
+    
+    public var content: String
+    
+    public init(title: String, link: String) {
+        let content: String = "[\(title)](\(link)"
+        self.content = content
+    }
+}
